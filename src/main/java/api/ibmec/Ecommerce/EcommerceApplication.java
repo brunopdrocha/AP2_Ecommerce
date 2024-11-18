876a3b140a0127800e81d196e367ea8b71384356
brunodprocha <brunodprocha@gmail.com>
2024-11-18 18:50:00 -0300

    Criado Repositorio e Config do CosmosDB

--- src/main/java/api/ibmec/Ecommerce/EcommerceApplication.java
+++ src/main/java/api/ibmec/Ecommerce/EcommerceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
+		//Desliga a validação do certificado
+		System.setProperty("javax.net.ssl.trustStore", "NUL");
+		System.setProperty("javax.net.ssl.trustStoreType", "Windows-ROOT");
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
